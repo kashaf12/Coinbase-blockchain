7e439a141eb89193c0bd53744516443ee31332bd
kashaf12 <kashafaahmed@gmail.com>
2022-02-27 02:22:11 +0530

    completed till modal

--- components/Header.js
+++ components/Header.js
@@ -1,50 +1,107 @@
-import React from 'react'
-import styled from 'styled-components'
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import Modal from "react-modal";
+import styled from "styled-components";
+import TransferModal from "./modal/TransferModal";
 
-function Header() {
-    return (
-        <Wrapper>
-            <Title>Assets</Title>
-            <ButtonsContainer>
-                <Button style={{ backgroundColor: '#3773f5', color: '#000' }}>
-                    Buy / Sell
-                </Button>
-                <Button>
-                    Send / Recieve
-                </Button>
-            </ButtonsContainer>
+Modal.setAppElement("#__next");
 
-        </Wrapper>
-    )
+function Header({ walletAddress, sanityTokens, connectWallet }) {
+  const router = useRouter();
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      transform: "translate(-50%, -50%)",
+      backgroundColor: "#0a0b0d",
+      padding: 0,
+      border: "none",
+    },
+    overlay: {
+      backgroundColor: "rgba(10, 11, 13, 0.75)",
+    },
+  };
+  return (
+    <Wrapper>
+      <Title>Assets</Title>
+      <ButtonsContainer>
+        <WalletLink>
+          <WalletLinkTitle>Wallet Connected</WalletLinkTitle>
+          <WalletAddress>
+            {walletAddress.slice(0, 7)}...{walletAddress.slice(35)}
+          </WalletAddress>
+        </WalletLink>
+        <Button style={{ backgroundColor: "#3773f5", color: "#000" }}>
+          Buy / Sell
+        </Button>
+        <Link href={"/?transfer=1"}>
+          <Button>Send / Receive</Button>
+        </Link>
+      </ButtonsContainer>
+      <Modal
+        isOpen={!!router.query.transfer}
+        onRequestClose={() => router.push("/")}
+        style={customStyles}
+      >
+        <TransferModal sanityTokens={sanityTokens} />
+      </Modal>
+    </Wrapper>
+  );
 }
 
-export default Header
+export default Header;
 
 const Wrapper = styled.div`
-width: calc(100% - 3rem);
-padding: 1rem 1.5rem;
-border-bottom: 1px solid #282b2f;
-display: flex;
-align-items: center;
-`
+  width: calc(100% - 3rem);
+  padding: 1rem 1.5rem;
+  border-bottom: 1px solid #282b2f;
+  display: flex;
+  align-items: center;
+`;
 
 const Title = styled.div`
-font-size: 2rem;
-font-weight: 600;
-flex: 1;
-`
+  font-size: 2rem;
+  font-weight: 600;
+  flex: 1;
+`;
 const ButtonsContainer = styled.div`
-display: flex;
-`
+  display: flex;
+`;
 
 const Button = styled.div`
-border: 1px solid #282b2f;
-padding: 0.8rem;
-font-size: 1.3rem;
-font-weight: 500;
-border-radius: 0.4rem;
-margin-right: 1rem;
-&:hover {
+  border: 1px solid #282b2f;
+  padding: 0.8rem;
+  font-size: 1.3rem;
+  font-weight: 500;
+  border-radius: 0.4rem;
+  margin-right: 1rem;
+  &:hover {
     cursor: pointer;
-}
-`
+  }
+`;
+
+const WalletLink = styled.div`
+  font-size: 0.8rem;
+  border: 1px solid #282b2f;
+  font-size: 1.2rem;
+  margin-right: 1rem;
+  padding: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+const WalletLinkTitle = styled.div`
+  font-size: 1.1rem;
+  margin-bottom: 0.3rem;
+  color: #27ad75;
+  font-weight: 600;
+`;
+
+const WalletAddress = styled.div`
+  font-size: 0.8rem;
+`;
--- components/Portfolio.js
+++ components/Portfolio.js
@@ -1,67 +1,107 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { coins } from "../static/coins";
 import Coin from "./Coin";
-function Portfolio() {
+import BalanceChart from "./BalanceChart";
+
+function Portfolio({ thirdWebTokens, sanityTokens, walletAddress }) {
+  const [walletBalance, setWalletBalance] = useState(0);
+  const tokenToUSD = {};
+
+  for (const token of sanityTokens) {
+    tokenToUSD[token.contractAddress] = Number(token.usdPrice);
+  }
+
+  useEffect(() => {
+    const calculateTotalBalance = async () => {
+      const totalBalance = await Promise.all(
+        thirdWebTokens.map(async (token) => {
+          const balance = await token.balanceOf(walletAddress);
+          console.log(balance);
+          return Number(balance.displayValue) * tokenToUSD[token.address];
+        })
+      );
+      setWalletBalance(totalBalance.reduce((acc, curr) => acc + curr, 0));
+    };
+
+    return calculateTotalBalance();
+  }, [thirdWebTokens, sanityTokens]);
+
   return (
-    <PortfolioTable>
-      <TableItem>
-        <Title>Your Assets</Title>
-      </TableItem>
-      <Divider />
-      <Table>
-        <TableItem>
-          <TableRow>
-            <div
-              style={{
-                flex: 3,
-              }}
-            >
-              Name
-            </div>
-            <div
-              style={{
-                flex: 2,
-              }}
-            >
-              Balance
-            </div>
-            <div
-              style={{
-                flex: 1,
-              }}
-            >
-              Price
-            </div>
-            <div
-              style={{
-                flex: 1,
-              }}
-            >
-              Allocation
-            </div>
-            <div
-              style={{
-                flex: 0,
-              }}
-            >
-              <BsThreeDotsVertical />
-            </div>
-          </TableRow>
-        </TableItem>
-        <Divider />
-        <div>
-          {coins.map((coin) => (
+    <Wrapper>
+      <Content>
+        <Chart>
+          <div>
+            <Balance>
+              <BalanceTitle>Portfolio Balance</BalanceTitle>
+              <BalanceValue>
+                {"$"}
+                {walletBalance.toLocaleString()}
+              </BalanceValue>
+            </Balance>
+          </div>
+          <BalanceChart />
+        </Chart>
+        <PortfolioTable>
+          <TableItem>
+            <Title>Your Assets</Title>
+          </TableItem>
+          <Divider />
+          <Table>
+            <TableItem>
+              <TableRow>
+                <div
+                  style={{
+                    flex: 3,
+                  }}
+                >
+                  Name
+                </div>
+                <div
+                  style={{
+                    flex: 2,
+                  }}
+                >
+                  Balance
+                </div>
+                <div
+                  style={{
+                    flex: 1,
+                  }}
+                >
+                  Price
+                </div>
+                <div
+                  style={{
+                    flex: 1,
+                  }}
+                >
+                  Allocation
+                </div>
+                <div
+                  style={{
+                    flex: 0,
+                  }}
+                >
+                  <BsThreeDotsVertical />
+                </div>
+              </TableRow>
+            </TableItem>
+            <Divider />
             <div>
-              <Coin coin={coin} />
-              {/* <h2>{coin.name}</h2> */}
-              <Divider />
+              {coins.map((coin) => (
+                <div>
+                  <Coin coin={coin} />
+                  {/* <h2>{coin.name}</h2> */}
+                  <Divider />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </Table>
-    </PortfolioTable>
+          </Table>
+        </PortfolioTable>
+      </Content>
+    </Wrapper>
   );
 }
 
@@ -79,6 +119,20 @@ const Content = styled.div`
   padding: 2rem 1rem;
 `;
 
+const Chart = styled.div`
+  border: 1px solid #282b2f;
+  padding: 1rem 2rem;
+`;
+const Balance = styled.div``;
+const BalanceTitle = styled.div`
+  color: #8a919e;
+  font-size: 0.9rem;
+`;
+const BalanceValue = styled.div`
+  font-size: 1.8rem;
+  font-weight: 700;
+  margin: 0.5rem 0;
+`;
 const PortfolioTable = styled.div`
   margin-top: 1rem;
   border: 1px solid #282b2f;
